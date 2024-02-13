8ae0275e9f51f5ab3974e97c1c8a588ea11a6ea8
Andrii <andpris527@gmail.com>
2024-02-13 22:58:32 +0200

    Change var to const, remove comments

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-// const usersRouter = require('./routes/users');
 const authorRouter = require("./routes/authors_router");
 
 const app = express();
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
@@ -25,20 +23,16 @@ app.use("/images", express.static("public/images"));
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
