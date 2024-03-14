4b1b61fee4b5f3ba1977d5d3fe57145ed1f0ccd0
Max <tyutyunnik.maks@gmail.com>
2024-03-14 22:38:44 +0200

    Resolved the problem with a link to the home page from authors pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "ToDo" });
+  res.render("index", { title: "ToDo", currentPage: req.path});
 });
 
 module.exports = router;
