fde68fdee092d9ab13daa85f33402881d36560e0
Andrii <andpris527@gmail.com>
2024-05-05 21:11:21 +0300

    Add task editing

--- routes/tasks_routers.js
+++ routes/tasks_routers.js
@@ -87,4 +87,29 @@ router.patch("/tasks", async (req, res) => {
     res.json({ redirect: "/" });
 });
 
+
+router.put("/tasks", async (req, res) => {
+    const description = req.body.description;
+    const priority = req.body.priority;
+    const finishDate = req.body.finishDate;
+    const creationTime = new Date();
+    const isDone = false;
+    const task_id = req.body.taskId;
+
+    const query =
+        "UPDATE tasks SET description=$1, priority=$2, finishDate=$3, creationTime=$4, isDone=$5 WHERE task_id=$6";
+    let result = await client.query(query, [
+        description,
+        priority,
+        finishDate,
+        creationTime,
+        isDone,
+        task_id,
+    ]);
+    console.log(result);
+
+    res.json({ redirect: "/" });
+});
+
+
 module.exports = router;
